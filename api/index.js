8b9ce5f1e49fdcae1100d92c12577d58b9b382c0
ShubhamNegi06 <shubh03uk06lko@outlook.com>
2024-09-01 21:17:46 +0530

    new

--- api/index.js
+++ api/index.js
@@ -6,8 +6,17 @@ const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 const usersRouter = require('./routes/userRoutes');
 const jobsRouter = require('./routes/jobsRoutes');
+const bodyParser = require("body-parser");
+const { body } = require('express-validator');
+dbconnection();
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next(); // Call the next middleware or route handler
+});
 
 app.use(express.json());
+app.use(bodyParser.json()),
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cors());
 
 app.use('/api/users', usersRouter);
@@ -15,6 +24,5 @@ app.use('/api/jobs', jobsRouter);
 
 
 app.listen(PORT, () => {
-       dbconnection();
   console.log(`Server is running on port ${PORT}`);
 });
